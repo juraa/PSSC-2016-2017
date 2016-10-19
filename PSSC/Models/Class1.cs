2d38c30c13389721882d83d7325695dbe3a877c7
juraa <jura.andrei94@gmail.com>
2016-10-19 16:19:47 +0300

    Update Class1.cs

--- PSSC/Models/Class1.cs
+++ PSSC/Models/Class1.cs
@@ -6,8 +6,50 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public class Class1
+    class Decanat
     {
-        //remove or alter this class, then implement your models.  
+        public string disciplina;
+        public string tip;
+        public int credite;
+        
+        
+        
+        public decanat(string dis, string t, int c)
+        {
+            diciplina = dis;
+            tip = t;
+            credite = c;
+        }
+        
+        public void print()
+        {
+            Console.writeline("Nume disciplina: " + disciplina );
+            Console.writeline("Tip evaluare: " +tip );
+            Console.writeline("Numar credite: " + credite );
+        }
+        
+        
+    }
+    class Profesor
+    {
+        
+        
+    }
+    
+    class Student
+    {
+        
+    }
+    class Facultate
+    {
+        public static void main (string[] args)
+        {
+            Console.Writeline();
+            decanat disciplina = new decanat("Algebra ", "Examen", 5);
+            
+            disciplina.print();
+                
+            
+        }
     }
 }
