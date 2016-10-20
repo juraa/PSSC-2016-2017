1c9aa08aab897bb179dc7c46e518c1181d49e2de
juraa <jura.andrei94@gmail.com>
2016-10-20 11:48:59 +0300

    Update Class1.cs

--- PSSC/Models/Class1.cs
+++ PSSC/Models/Class1.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    class Decanat
+    class Facultate
     {
+      
+        
+        class decanat
+        {
+    
         public string disciplina;
         public string tip;
         public int credite;
@@ -15,41 +20,75 @@ namespace Models
         
         
         public decanat(string dis, string t, int c)
-        {
+            {
             diciplina = dis;
             tip = t;
             credite = c;
-        }
+            }
         
         public void print()
-        {
+            {
             Console.writeline("Nume disciplina: " + disciplina );
             Console.writeline("Tip evaluare: " +tip );
             Console.writeline("Numar credite: " + credite );
+            }
+        
+    
         }
         
+        class Profesor : decanat
+        {
+            public float nota;
+            public float nota1;
+            public float nota2;
+            public float marire;
+      
         
-    }
-    class Profesor
-    {
+            
+             public Profesor(string _dis, string _t, int _c)
+             {
+                diciplina = _dis;
+                tip = _t;
+                credite = _c;
+                
+                nota = _nota;
+                nota1 = _nota1;
+                nota2 = _nota2;
+                marire = _marire;
+             }
         
+        public void print()
+            {
+                Console.writeline("Nume disciplina: " + disciplina );
+                Console.writeline("Tip evaluare: " +tip );
+                Console.writeline("Numar credite: " + credite );
+                
+                Console.writeline("Nota examen: " + nota );
+                Console.writeline("Nota distribuita1: " + nota1 );
+                Console.writeline("Nota distribuita 2: " + nota2 );
+                Console.writeline("Nota dupa marire: " + marire );
+            }
+        }
+       
         
-    }
     
-    class Student
-    {
-        
-    }
-    class Facultate
-    {
+  
+ 
         public static void main (string[] args)
         {
             Console.Writeline();
-            decanat disciplina = new decanat("Algebra ", "Examen", 5);
-            
-            disciplina.print();
+            decanat disciplina1 = new decanat("Algebra ", "Examen", 5);
+            disciplina1.nota=6;
+            disciplina1.print();
+            Console.Writeline();
+            decanat disciplina2 = new decanat("Fizica ", "Examen distribuit", 4);
+            disciplina2.nota1=5;
+            disciplina2.nota2=7;
+            disciplina2.marire=disciplina2.nota1 + 2;
+            disciplina2.print();
                 
             
         }
     }
+    
 }
